63b2e7fa23451d085aabb32a54d0e0f305cea553
choi00105 <chleo77@naver.com>
2022-12-13 10:22:26 +0900

    feat: Todo submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Todo from './components/Todo';
+import AddTodo from './components/AddTodo';
 
 const App = () => {
   const [todoItems, setTodoItems] = useState([
@@ -19,13 +20,46 @@ const App = () => {
       done: true,
     },
   ]);
+  const todoId = useRef(4); //useRef를 로컬변수(랜더링 되어도 값이 유지됨)로 사용 -> db랑 연결된 느낌을줌 -> db연결 하면 지워
+
+// AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
+// 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
+// => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
+
+  // const addItem = (newItem) => {
+  const addItem = (newItem) => {
+    //...
+    // newItem - {id:todoItems.length + 1 title: 받아오기 done:false} 객체형식 
+    // setTodoItems()
+
+    newItem.id = todoId.current++; // 키를 위한 id 설정
+    newItem.done = false; // done 초기화
+    // 기존 todoItems를 유지하고, 새로운 newItem을 추가
+    setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
+
+    // const newItem = todoItems.concat({
+    //   id: todoItems.length + 1,
+    //   title: newTitle,
+    //   done: false,
+    // });
+    // setTodoItems(newItem)
+
+  };
 
   return (
     <div className="App">
+      {console.log('todoItems', todoItems)}
+      {console.log(todoItems[0])}
+      
+      <AddTodo
+        addItem={addItem}
+      ></AddTodo>
+
       {todoItems.map((item) => {
         // console.log(item); // {id: 1, title: 'My Todo1', done: false}
         return <Todo key={item.id} item={item}></Todo>;
       })}
+      
     </div>
   );
 };
---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const AddTodo = ({ addItem }) => {
+  // const { addItem } = props;  // props로 받는다면 이거 해줘야 함수 사용 가능
+  
+  // 사용자 입력을 저장할 객체
+  // id title done 에 대한 정보를 저장해서 객체로
+  
+  const [todoItem, setTodoItem] = useState({
+    title: '',
+  });
+
+  const onButtonClick = () => {
+    //props로 받아온 addItem 함수 실행
+    addItem(todoItem); // {title: 'input 입력값'}
+    setTodoItem({ title: ''});
+    // setTodoItem(this.value)
+    // return this.value;
+  };
+
+  const onEnterKeyPress = (e) =>{
+    if (e.key === 'Enter') {
+      onButtonClick();
+    }
+  };
+
+  return (<div className="AddTodo">
+    <input type="text" 
+    value={todoItem.title} 
+    placeholder="Add your new todo"
+    onChange={(e) => setTodoItem({ title: e.target.value})}
+    onKeyPress={onEnterKeyPress}></input>
+
+    <button onClick={onButtonClick} >Add</button>
+  </div>
+  )
+}
+
+export default AddTodo;
