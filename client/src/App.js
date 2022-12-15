c5d8e953d602258576281189f22eee50cf18cd54
choi00105 <chleo77@naver.com>
2022-12-15 11:25:01 +0900

    feat: add del

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import Todo from './components/Todo';
 import AddTodo from './components/AddTodo';
 
-
 // import './styles/SassDh.scss';
 import './styles/App.scss'
 
@@ -24,30 +23,37 @@ const App = () => {
     getTodos();
   }, []) //빈배열을 불러옴
 
-
-
-
-
-
 // AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems(state)에 접근 불가능
 // 상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능
 // => App 컴포넌트에 addItem() 함수를 정의하고, 해당 함수를 AddTodo props로 넘겨야 함
 
   // const addItem = (newItem) => {
-  const addItem = (newItem) => {
+  const addItem = async (newItem) => {
+    // axios.post(url, data)
 
+    console.log(newItem) // {title: '입렵값'}
+    let add = await axios.post('http://localhost:8081/todo', newItem);
+    console.log(add.data);
+
+    // [back 연결 전]
     // newItem.id = todoId.current++; // 키를 위한 id 설정
-    newItem.done = false; // done 초기화
+    // newItem.done = false; // done 초기화
     // 기존 todoItems를 유지하고, 새로운 newItem을 추가
     setTodoItems([...todoItems, newItem]); // setTodoItems(todoItems.concat(newItem))
+
+    // setTodoItems([...todoItems, add.data]); // setTodoItems(todoItems.concat(newItem))
   };
 
   // 전체 Todo 리스트(todoItems)는 App 컴포넌트에서 관리하고 있으므로
   // delItem() 함수는 App 컴포넌트에 작성해야함
-  const delItem = (targetItem) => {
-    console.log(targetItem);
+  const delItem = async (targetItem) => {
+    console.log(targetItem.id);
     // filter method 이용
-    let newtodoItems = todoItems.filter((item) => targetItem.id !== item.id )
+    let del = await axios.delete(`http://localhost:8081/todo/${targetItem.id}` );
+    console.log('보여줘', del);
+    
+
+    let newtodoItems = todoItems.filter((item) => targetItem.id !== item.id );
     console.log('newtodo', newtodoItems);
     setTodoItems(newtodoItems);
   };
